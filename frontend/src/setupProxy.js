8db1037757d9083187bbbb2102f0b47492b11fb2
anilkusc <anilkuscu95@gmail.com>
2020-10-22 17:49:22 +0300

    test

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware');
+/*const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(proxy('/backend', { target: 'http://localhost:80' }));
   app.use(proxy('/ws', { target: 'ws://localhost:80', ws: true }));
-};
+};*/
