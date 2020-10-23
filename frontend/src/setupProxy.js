6fcba1166a9447c822b8d12b71b5e034b011fad8
anilkusc <anilkuscu95@gmail.com>
2020-10-24 00:43:43 +0300

    websocket proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,5 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(proxy('/backend/', { target: 'http://localhost:80' }));
-  app.use(proxy('/ws/', { target: 'ws://localhost:80/ws/status/', ws: true }));
+  app.use(proxy('/ws/status/', { target: 'ws://localhost:80', ws: true }));
 };
